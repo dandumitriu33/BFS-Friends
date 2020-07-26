86f5cef59dc651009191cb7ec7c17f347671156e
László Molnár <laszlo.molnar@codecool.com>
2020-07-26 06:10:59 +0200

    Initial commit

---
+++ BFS-c-sharp/Model/UserNode.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace BFS_c_sharp.Model
+{
+    public class UserNode
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        private readonly HashSet<UserNode> _friends = new HashSet<UserNode>();
+
+        public HashSet<UserNode> Friends
+        {
+            get { return _friends; }
+        }
+
+
+        public UserNode() { }
+
+        public UserNode(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public void AddFriend(UserNode friend)
+        {
+            Friends.Add(friend);
+            friend.Friends.Add(this);
+        }
+
+        public override string ToString()
+        {
+            return FirstName + " " + LastName + "(" + Friends.Count + ")";
+        }
+    }
+}
---
+++ BFS-c-sharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using BFS_c_sharp.Model;
+using System;
+using System.Collections.Generic;
+
+namespace BFS_c_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RandomDataGenerator generator = new RandomDataGenerator();
+            List<UserNode> users = generator.Generate();
+
+            foreach (var user in users)
+            {
+                Console.WriteLine(user);
+            }
+
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+    }
+}
